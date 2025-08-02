098cb83594cbc91505ee83393d18c55ca15d2f36
Rizky Mahendra <56747067+Rizkyemha@users.noreply.github.com>
2025-08-02 13:08:48 +0700

    create table playlist_activities and enum action

---
+++ migrations/1754114135400_create-table-playlist-activities.js
@@ -0,0 +1,38 @@
+exports.up = (pgm) => {
+	pgm.createType("action_enum", ["add", "delete"]);
+
+	pgm.createTable("playlist_activities", {
+		id: {
+			type: "VARCHAR(50)",
+			primaryKey: true,
+		},
+		action: {
+			type: "action_enum",
+			notNull: true,
+		},
+		playlist_id: {
+			type: "VARCHAR(50)",
+			notNull: true,
+			references: "playlist(id)",
+			referencesConstraintName:
+				"fk_playlist_activities.playlist_id_playlist.id",
+			onDelete: "CASCADE",
+		},
+		user_id: {
+			type: "VARCHAR(50)",
+			notNull: true,
+			references: "users(id)",
+			referencesConstraintName: "fk_playlist_activities.user_id_users.id",
+			onDelete: "CASCADE",
+		},
+		time: {
+			type: "TIMESTAMP",
+			notNull: true,
+		},
+	});
+};
+
+exports.down = (pgm) => {
+	pgm.dropTable("playlist_activities");
+	pgm.dropType("action_enum");
+};
